c858380b8464ccfcc80a4b28a8a86fe378aa74d8
Julia Kopysova <julykopysova97@gmail.com>
2020-01-23 20:50:59 +0200

    Cart

--- cart/cart.py
+++ cart/cart.py
@@ -2,23 +2,42 @@ from decimal import Decimal
 from django.conf import settings
 from polls.models import Type, Item
 from .forms import CartAddItemForm
+import logging, logging.config
+import sys
 
 class Cart(object):
+    LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+        }
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO'
+    }
+    }
+    logging.config.dictConfig(LOGGING)
 
     def __init__(self, request):
         #Initialize the cart.
+        logging.info('Initialize')
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
+        logging.info(cart)
         if not cart:
+            logging.info('In if')
             # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
     def add(self, item):
         item_id = str(item.id)
-        self.cart[item_id] = {'type_tiсket': {'price': str(item.type_ticket.price),
-                                             'days':str(item.type_ticket.days),
-                                             'type':str(item.type_ticket.type)},
+        self.cart[item_id] = {'price': str(item.type_ticket.price),
+                              'days':str(item.type_ticket.days),
+                              'type':str(item.type_ticket.type),
                               #'price': str(item.type_ticket.price),
                               'name_person':str(item.name_person),
                               'surname_person': str(item.surname_person),
@@ -45,10 +64,10 @@ class Cart(object):
         for item in items:
             self.cart[str(item.id)]['item'] = item
         for i in self.cart.values(): #значения в словаре
-            i['type_ticket']['price'] = Decimal(i['type_ticket']['price'])
+            i['price'] = Decimal(i['price'])
             yield i #вернeт генератор
     def get_total_price(self):
-        return sum(Decimal(i['type_ticket']['price'])  for i in self.cart.values())
+        return sum(Decimal(i['price'])  for i in self.cart.values())
     def clear(self):
     # remove cart from session
         del self.session[settings.CART_SESSION_ID]
